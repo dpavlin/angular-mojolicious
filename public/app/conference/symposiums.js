4d82358d6579a2553f6d276c70b90563444bcc7f
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-11-11 19:22:51 +0100

    split controller and rename it

---
+++ public/app/conference/symposiums.js
@@ -0,0 +1,20 @@
+
+Symposiums.$inject = ['$xhr', '$route']; 
+
+function Symposiums(xhr, route){ 
+
+	this.route = route; 
+	this.xhr = xhr; 
+
+	var data = {}
+	this.data = data;
+
+	this.xhr("JSON"
+		, "http://localhost:5984/conference/_design/symposium/_view/work_nr%2Ctitle?callback=JSON_CALLBACK"
+		, function(code, response){ 
+			console.log(code, response, data);
+			data.response = response;
+		}
+	); 
+}
+
