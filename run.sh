c245746b1a13b0f3db01b92f630f231669aa3784
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-11-19 18:40:51 +0100

    simple run/update script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -x
+
+if [ ! -z "$1" ] ; then
+	git pull
+	git submodule update
+	test -f public/angular/build/angular.js && ( cd public/angular && rake compile )
+fi
+
+./angular-server.pl daemon --reload #--listen 'http://*:3000'
+
