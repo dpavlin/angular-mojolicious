78fa6f3ee1af08858601029a443fe88ad7a873eb
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-11-29 13:07:20 +0100

    fix angular compile

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ ! -z "$1" ] ; then
 	git pull
 	git submodule init
 	git submodule update
-	test -f public/angular/build/angular.js && ( cd public/angular && rake compile )
+	test -f public/angular/build/angular.js || ( cd public/angular && rake compile )
 fi
 
 ./angular-server.pl daemon --reload #--listen 'http://*:3000'
